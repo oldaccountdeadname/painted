b43be4d136faf12a01ff3228842993d248990553
lincoln auster [they/them] <lincolnauster@gmail.com>
2021-12-07 07:51:58 -0700

    [minor] fix flake.nix formatting

--- flake.nix
+++ flake.nix
@@ -1,23 +1,25 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
-  outputs = { self, nixpkgs }: let pkgs = import nixpkgs { system = "x86_64-linux"; }; in {
-    defaultPackage.x86_64-linux = pkgs.buildGoModule {
-      name = "painted";
-      version = "v0.1.1";
+  outputs = { self, nixpkgs }:
+    let pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in {
+      defaultPackage.x86_64-linux = pkgs.buildGoModule {
+        name = "painted";
+        version = "v0.1.1";
 
-      src = builtins.filterSource
-        (path: type: baseNameOf path != "contrib")
-        ./.;
+        src = builtins.filterSource
+          (path: type: baseNameOf path != "contrib")
+          ./.;
 
-      vendorSha256 = "sha256-TtaXy5gLcHguw1OnFIsb/BDyNKM3A7ZxVk0mIxVWssg";
+        vendorSha256 = "sha256-TtaXy5gLcHguw1OnFIsb/BDyNKM3A7ZxVk0mIxVWssg";
+      };
+
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = [ pkgs.go pkgs.libnotify ];
+        shellHook = ''
+          ln -sf ../../.githooks/pre-commit .git/hooks/pre-commit
+        '';
+      };
     };
-
-    devShell.x86_64-linux = pkgs.mkShell {
-      buildInputs = [ pkgs.go pkgs.libnotify ];
-      shellHook = ''
-        ln -sf ../../.githooks/pre-commit .git/hooks/pre-commit
-      '';
-    };
-  };
 }
