151ca8bd36d8393e0c48506355bd201592cf01c1
lincoln auster [they/them] <lincolnauster@gmail.com>
2022-01-24 14:54:44 -0700

    [go fmt]

--- pkg/painted/ioutil_bsd.go
+++ pkg/painted/ioutil_bsd.go
@@ -1,4 +1,5 @@
-//+build freebsd openbsd
+//go:build freebsd || openbsd
+// +build freebsd openbsd
 
 package painted
 
@@ -8,23 +9,23 @@ import "syscall"
 //
 // Errors are ignored.
 func blockUntilModify(f string) {
-	kq , _ := syscall.Kqueue()
+	kq, _ := syscall.Kqueue()
 	fd, _ := syscall.Open(f, syscall.O_RDONLY, 0)
 	// seek to the end to only register new writes
 	// FIXME: this isn't atomic.
 	syscall.Seek(fd, 0, 2)
 
 	ke := syscall.Kevent_t{
-		Ident: uint64(fd),
+		Ident:  uint64(fd),
 		Filter: syscall.EVFILT_READ,
-		Flags: syscall.EV_ADD | syscall.EV_ONESHOT,
+		Flags:  syscall.EV_ADD | syscall.EV_ONESHOT,
 		Fflags: 0,
-		Data: 0,
-		Udata: nil,
+		Data:   0,
+		Udata:  nil,
 	}
 
 	kchange_list := []syscall.Kevent_t{ke}
-	kevent_list  := make([]syscall.Kevent_t, 1);
+	kevent_list := make([]syscall.Kevent_t, 1)
 
 	syscall.Kevent(kq, kchange_list, kevent_list, nil)
 }
