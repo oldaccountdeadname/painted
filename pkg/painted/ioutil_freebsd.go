edd4bc96458a3689e4f8065d034aaa62ca9b2d4a
lincoln auster [they/them] <lincolnauster@gmail.com>
2022-01-23 23:14:33 -0700

    support FreeBSD with kqueue
    This is duplicates most of the ioutil code to support freebsd via the
    kqueue interface. Most of the ioutil code isn't platform specific, and
    should be refactored into a common implementation soon.
    
    Fixes #4.

---
+++ pkg/painted/ioutil_freebsd.go
@@ -0,0 +1,84 @@
+//+build freebsd
+
+package painted
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"syscall"
+)
+
+type Reader struct {
+	File io.Reader
+	Path string
+}
+
+type Writer struct {
+	File io.Writer
+	Path string
+}
+
+type Io struct {
+	Reader Reader
+	Writer Writer
+}
+
+/* reading helpers */
+
+// Return a closure that continues to read the next line. On EOF, the returned
+// function blocks until the file has been modified, and then tries again.
+func (i *Io) Lines() func() (string, error) {
+	f := bufio.NewReader(i.Reader.File)
+
+	return func() (string, error) {
+		for {
+			out, err := f.ReadString('\n')
+			if err == io.EOF {
+				blockUntilModify(i.Reader.Path)
+			} else if err != nil {
+				return "", err
+			} else {
+				return out, nil
+			}
+		}
+	}
+}
+
+/* writing helpers */
+
+func (i *Io) Write(s string) {
+	i.Writer.File.Write([]byte(s))
+}
+
+func (i *Io) Writef(f string, v ...interface{}) {
+	i.Write(fmt.Sprintf(f, v...))
+}
+
+/* misc helpers */
+
+// Use kqueue to watch a given file path and `read` (block until an event
+// occurs). See inotify(2). This is a linux-specific syscall.
+//
+// Errors are ignored.
+func blockUntilModify(f string) {
+	kq , _ := syscall.Kqueue()
+	fd, _ := syscall.Open(f, syscall.O_RDONLY, 0)
+	// seek to the end to only register new writes
+	// FIXME: this isn't atomic.
+	syscall.Seek(fd, 0, 2)
+
+	ke := syscall.Kevent_t{
+		Ident: uint64(fd),
+		Filter: syscall.EVFILT_READ,
+		Flags: syscall.EV_ADD | syscall.EV_ONESHOT,
+		Fflags: 0,
+		Data: 0,
+		Udata: nil,
+	}
+
+	kchange_list := []syscall.Kevent_t{ke}
+	kevent_list  := make([]syscall.Kevent_t, 1);
+
+	syscall.Kevent(kq, kchange_list, kevent_list, nil)
+}
